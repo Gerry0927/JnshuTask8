055e73d633441bb88fe804695db433b75c11f718
Gary <zzhdroid@gmail.com>
2020-02-29 17:51:18 +0800

    finish task9

--- NotifyModule/src/main/java/com/jnshu/notify/NotifyApplication1.java
+++ NotifyModule/src/main/java/com/jnshu/notify/NotifyApplication1.java
@@ -7,20 +7,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = "com.jnshu")
-public class NotifyApplication
-        extends SpringBootServletInitializer implements WebApplicationInitializer {
+@ImportResource("classpath:dubbo.xml")
+public class NotifyApplication {
+        //extends SpringBootServletInitializer implements WebApplicationInitializer {
     static Logger logger = LoggerFactory.getLogger(NotifyApplication.class);
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(NotifyApplication.class, args);
-//    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(NotifyApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(NotifyApplication.class, args);
     }
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(NotifyApplication.class);
+//    }
 }
--- TaskModule/src/main/java/com/gerry/task/TaskApplication.java
+++ TaskModule/src/main/java/com/gerry/task/TaskApplication.java
@@ -8,22 +8,24 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.WebApplicationInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.gerry.task.dao")
+@ImportResource("classpath:dubbo.xml")
 //@ComponentScan(basePackages = {"com.gerry","com.jnshu"})
-public class TaskApplication
-        extends SpringBootServletInitializer implements WebApplicationInitializer {
-    static Logger logger = LoggerFactory.getLogger(TaskApplication.class);
+public class TaskApplication1 {
+        //extends SpringBootServletInitializer implements WebApplicationInitializer {
+    static Logger logger = LoggerFactory.getLogger(TaskApplication1.class);
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(TaskApplication.class, args);
-//    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(TaskApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication1.class, args);
     }
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(TaskApplication.class);
+//    }
 }
